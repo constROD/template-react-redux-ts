596b4cf6905571947dc46376d9f5cf79db4d3c79
Rodrick Alcantara <60320390+constROD@users.noreply.github.com>
2020-10-22 12:23:40 +0800

    Update store.ts

--- src/shared/redux/store.ts
+++ src/shared/redux/store.ts
@@ -2,11 +2,11 @@ import { applyMiddleware, compose, createStore } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import { CONFIG } from "shared/configs/app";
-import reducers from "./reducers";
+import { rootReducers } from "./reducers";
 
 const middlewares =
   CONFIG.ZONE === "production"
     ? compose(applyMiddleware(thunk))
     : compose(applyMiddleware(thunk, logger));
 
-export default createStore(reducers, middlewares);
+export default createStore(rootReducers, middlewares);
