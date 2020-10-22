97b3e9999ab77b5c26919cd423194f62ad5acc85
Rodrick Alcantara <60320390+constROD@users.noreply.github.com>
2020-10-22 12:14:28 +0800

    Update users.ts

--- src/shared/redux/reducers/users.ts
+++ src/shared/redux/reducers/users.ts
@@ -1,11 +1,11 @@
 import { DEFAULT_USERS } from "shared/constants/redux/defaults";
-import { ActionType } from "shared/models/redux/store";
-import { CurrentUser } from "shared/models/redux/users";
+import { IActionType } from "shared/models/redux/store";
+import { ICurrentUser } from "shared/models/redux/users";
 import { SET_CURRENT_USER } from "../../constants/redux/types";
 
-const usersReducer = (
+export const usersReducer = (
   state = DEFAULT_USERS,
-  action: ActionType<CurrentUser>
+  action: IActionType<ICurrentUser>
 ) => {
   switch (action.type) {
     case SET_CURRENT_USER:
@@ -19,5 +19,3 @@ const usersReducer = (
       return state;
   }
 };
-
-export default usersReducer;
