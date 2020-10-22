3338bf0d9e40d9ce6c019100797c5acea927c9f2
Rodrick Alcantara <60320390+constROD@users.noreply.github.com>
2020-10-22 12:11:27 +0800

    Update users.ts

--- src/shared/redux/actions/users.ts
+++ src/shared/redux/actions/users.ts
@@ -1,10 +1,10 @@
-import { ActionType } from "shared/models/redux/store";
-import { CurrentUser } from "shared/models/redux/users";
+import { IActionType } from "shared/models/redux/store";
+import { ICurrentUser } from "shared/models/redux/users";
 import { SET_CURRENT_USER } from "../../constants/redux/types";
 
 export const setCurrentUser = (
-  payload: CurrentUser
-): ActionType<CurrentUser> => {
+  payload: ICurrentUser
+): IActionType<ICurrentUser> => {
   return {
     type: SET_CURRENT_USER,
     payload,
